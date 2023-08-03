ff9bc35817a3736c79287a22cac18dbf036e7187
Nathan Krasney <natankrasney@gmail.com>
2023-08-03 13:25:13 +0300

    persist unit test is ok

---
+++ system/final/test/persistence.test.ts
@@ -0,0 +1,49 @@
+import { test, expect } from "vitest";
+import persist from '../src/lib/persistence'
+import { ITask } from "../src/types/i-task";
+
+test('localStorage is not null',()=>{
+    expect(localStorage).not.toBe(null)
+})
+
+test('localStorage set \ get are working ok',()=>{
+    localStorage.setItem('key1','val1');
+
+    expect(localStorage.getItem('key1')).toBe('val1')
+})
+
+test('persist.load() from empty storage in []',()=>{
+    const tasks = persist.load();
+    expect(tasks.length).toBe(0);
+})
+
+test('save one task and load one task -> is ok',()=>{
+    const task1 : ITask ={
+        action: "action1",
+        payload: {foo : 1}
+    }
+
+    const tasks : ITask [] = [task1];
+    persist.save(tasks);
+    const tasksLoaded = persist.load();
+    expect(tasksLoaded.length).toBe(1);
+    expect(tasksLoaded[0].payload).toEqual({foo:1})
+    expect(tasksLoaded).toEqual(tasks);
+})
+
+test('save two tasks and load two tasks -> is ok',()=>{
+    const task1 : ITask ={
+        action: "action1",
+        payload: {foo : 1}
+    }
+    const task2 : ITask ={
+        action: "action2",
+        payload: {bar : 2}
+    }
+
+    const tasks : ITask [] = [task1 , task2];
+    persist.save(tasks);
+    const tasksLoaded = persist.load();
+    expect(tasksLoaded.length).toBe(2);
+    expect(tasksLoaded).toEqual(tasks);
+})
--- system/final/vite.config.ts
+++ system/final/vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     globals: true,
-    environment: 'jsdom',
+    environment:'jsdom',
     coverage: {
       provider: 'istanbul' // or 'v8'
     },
