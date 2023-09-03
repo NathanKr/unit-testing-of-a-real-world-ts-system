b74a60ae241e463601a8b7ed71a572c22d2b6245
Nathan Krasney <natankrasney@gmail.com>
2023-09-03 10:58:56 +0300

    vitest ignore test-jest

--- system/final/vite.config.ts
+++ system/final/vite.config.ts
@@ -1,16 +1,23 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vitest/config'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   test: {
-    setupFiles: './test/setup.ts',
+    exclude: [
+      "**/test-jest/**",
+      "**/node_modules/**",
+      "**/dist/**",
+      "**/cypress/**",
+      "**/.{idea,git,cache,output,temp}/**",
+      "**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*",
+    ],
+    setupFiles: "./test/setup.ts",
     globals: true,
-    environment:'jsdom',
+    environment: "jsdom",
     coverage: {
-      provider: 'istanbul' // or 'v8'
+      provider: "istanbul", // or 'v8'
     },
   },
-})
-
+});
