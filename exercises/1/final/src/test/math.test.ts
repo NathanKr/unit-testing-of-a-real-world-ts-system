7b7e55292712d1b7e485a6ebcfeb939b0f0deaa8
Nathan Krasney <natankrasney@gmail.com>
2023-09-27 13:20:36 +0300

    exc 1 ok

---
+++ exercises/1/final/src/test/math.test.ts
@@ -0,0 +1,19 @@
+import {expect, test} from 'vitest'
+import { add, calculateAverage, mul } from '../math'
+
+test('add(1,2) -> 3',()=>{
+    expect(add(1,2)).toBe(3)
+})
+
+
+test('mul(2,3) => 6',()=>{
+    expect(mul(2,3)).toBe(6)
+})
+
+test('calculateAverage is ok',()=>{
+    expect(calculateAverage([])).toBe(NaN);
+    expect(calculateAverage([1])).toBe(1)
+    expect(calculateAverage([1, 2, 3])).toBe(2);
+    expect(calculateAverage([-1, 4, 3])).toBe(2);
+    expect(calculateAverage([-1, 4, -3])).toBe(0);
+})
