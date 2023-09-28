9a44f69ea933d23c24103532dc397b66d1c2cb29
Nathan Krasney <natankrasney@gmail.com>
2023-09-28 21:16:24 +0300

    add few imaged to figs

---
+++ exercises/3/final/vite.config.ts
@@ -0,0 +1,10 @@
+/// <reference types="vitest" />
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    coverage: {
+      provider: "istanbul", // or 'v8'
+    },
+  },
+});
