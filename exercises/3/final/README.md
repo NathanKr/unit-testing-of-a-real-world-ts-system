3ea660383bfe41e7d9e7912f908abe6480de61d8
Nathan Krasney <natankrasney@gmail.com>
2023-10-01 14:47:31 +0300

    exc 3

---
+++ exercises/3/final/README.md
@@ -0,0 +1,17 @@
+<h2>Exercise 3</h2>
+
+<h3>Subject</h3> 
+coverage test - Invoke code coverage on Excercise 2
+
+<h3>Section 5 </h3>
+Code coverage
+
+<h3>Your tasks</h3>
+<ol>
+<li>invoke test coverage</li>
+<li>* fix the source file and add tests such that the coverage test will be 100%</li>
+</ol>
+
+
+<h3>Guidelines</h3>
+use the dictionary file under docs
---
+++ exercises/3/final/test/grade-calculator.test.ts
@@ -0,0 +1,22 @@
+import { expect, test } from "vitest";
+import { calculateAverage, parseCSV } from "../src/grades-calculator";
+
+test("calculateAverage is ok", () => {
+  expect(calculateAverage([])).toBe(NaN);
+  expect(calculateAverage([1])).toBe(1);
+  expect(calculateAverage([1, 2, 3])).toBe(2);
+  expect(calculateAverage([-1, 4, 3])).toBe(2);
+  expect(calculateAverage([-1, 4, -3])).toBe(0);
+});
+
+test("parseCSV is ok", () => {
+  expect(parseCSV("")).toEqual([]);
+  expect(parseCSV("John Deer , 80")).toEqual([80]);
+  expect(parseCSV("John Deer , 80\nMike Tyson , 22")).toEqual([80, 22]);
+  expect(
+    parseCSV("John Deer , 80\nMike Tyson , 22\nBarak Obama , 100")
+  ).toEqual([80, 22, 100]);
+  expect(
+    parseCSV("John Deer , 80\nMike Tyson , aa\nBarak Obama , 100")
+  ).toEqual([80,  100]);
+});
