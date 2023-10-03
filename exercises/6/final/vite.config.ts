fc60a49449d98914a69835cbf9246e8d3dd71c5c
Nathan Krasney <natankrasney@gmail.com>
2023-10-03 11:51:19 +0300

    ex6

---
+++ exercises/6/final/vite.config.ts
@@ -0,0 +1,9 @@
+/// <reference types="vitest" />
+import { defineConfig } from "vitest/config";
+export default defineConfig({
+  test: {
+    coverage: {
+      provider: "istanbul", // or 'v8'
+    },
+  },
+});
